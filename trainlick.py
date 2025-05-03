4d100ecba45ccf88788717781e28ea003416c580
DartAiden <adartley123@gmail.com>
2025-05-03 05:38:49 -0400

    m

--- trainlick.py
+++ trainlick.py
@@ -9,26 +9,73 @@ import matplotlib.pyplot as plt
 from skimage import io, transform
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
-import io
+from torchvision.io import read_image
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 
-class licking(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.landmarks_frame = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
+
+class createDataset(Dataset):
+    def __init__(self, data, labels):
+        self.labels = pd.read_csv(labels)
+        self.data = data
     def __len__(self):
-        return len(self.landmarks_frame)
+        return len(self.labels)
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        img_path = os.path.join(self.data, self.labels.iloc[idx, 0])
+        image = read_image(img_path).float() / 255.0 
+        label = self.labels.iloc[idx,1]
+        return image,label
+    
 
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        sample = {'image': image}
 
-        if self.transform:
-            sample = self.transform(sample)
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-        return sample
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+licklabels = 'lick_annotations.csv'
+lickdir = r'C:\Users\adart\Documents\mouse-licking-analysis\licking_combined'
+lickdataset = createDataset(lickdir, licklabels)
+
+lick_dataloader = DataLoader(lickdataset, batch_size=64, shuffle=True)
+train_features, train_labels = next(iter(lickdataset))
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(lick_dataloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+PATH = './licktrain.pth'
+torch.save(net.state_dict(), PATH)
