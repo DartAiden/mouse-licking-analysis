3b25b96d8d9435cbfc46a8ea0cec79fb73685fef
DartAiden <85815329+DartAiden@users.noreply.github.com>
2025-05-10 22:32:44 -0400

    Update trainlick.py

--- trainlick.py
+++ trainlick.py
@@ -61,17 +61,12 @@ train_features, train_labels = next(iter(lickdataset))
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(2):  
 
     running_loss = 0.0
     for i, data in enumerate(lick_dataloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-
-        # zero the parameter gradients
         optimizer.zero_grad()
-
-        # forward + backward + optimize
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
